bceeb41bd1b2f7e0e77b5c00e1dfea8c4745b6b6
willis7 <sion5@hotmail.co.uk>
2015-03-24 19:59:26 +0000

    Used a template to clean up the code. Added the templateHandler type and ServeHTTP method to load the source file, compile the template and execute it, and write the output to the specified http.ResponseWriter object.

--- main.go
+++ main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					Lets chat!
-				</body>
-			</html>
-		`))
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(rw, nil)
+}
+
+func main() {
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// start the webserver
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
